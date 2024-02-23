98856e4bad66a07c60c69a3b439382f0a8a83aed
Hsb1122 <153083377+Hsb1122@users.noreply.github.com>
2024-02-24 02:41:26 +0500

    magic

--- main.py
+++ main.py
@@ -1,69 +1,37 @@
-# class Tiger:
-#     def Tiger_attributes(self, Tiger_age, Tiger_color, Tiger_gender, Tiger_breed, Tiger_mood):
-#         self.Tiger_age = Tiger_age
-#         self.Tiger_color = Tiger_color
-#         self.Tiger_gender = Tiger_gender
-#         self.Tiger_breed = Tiger_breed
-#         self.Tiger_mood = Tiger_mood
-
-
-# def our_tiger(self, tiger_name, tiger_diet_plan, tiger_outing, tiger_env):
-#     self.tiger_name = tiger_name
-#     self.tiger_diet_plan = tiger_diet_plan
-#     self.tiger_outing = tiger_outing
-#     self.tiger_env = tiger_env
-
-
-# def tiger_memory_address(self):
-#     print(f'Tiger is located in this {id(self)} address')
-
-
-# def tiger_report(self):
-#     print(f'''
-#     Tiger Report is
-#     Tiger name is {self.tiger_name}
-#     Tiger diet plan is {self.tiger_diet_plan}
-#     Tiger Environment is {self.tiger_env}
-#     ''')
-
-#     obj1: Tiger = Tiger()
-#     obj1.Tiger_attributes('2 Months', 'black', 'male', 'chicken', 'agressive')
-#     obj1.our_tiger('jack', '3 / 3', 'Commercial', 'garden')
-#     obj1.tiger_memory_address()
-#     obj1.tiger_report()
-#     print(f'obj1 Address is {id(obj1)}')
-
-
-# Chat GPT
 class Tiger:
-    def Tiger_attributes(self, Tiger_age, Tiger_color, Tiger_gender, Tiger_breed, Tiger_mood):
-        self.Tiger_age = Tiger_age
-        self.Tiger_color = Tiger_color
-        self.Tiger_gender = Tiger_gender
-        self.Tiger_breed = Tiger_breed
-        self.Tiger_mood = Tiger_mood
+    def __init__(self):
+        pass
 
-    def our_tiger(self, tiger_name, tiger_diet_plan, tiger_outing, tiger_env):
-        self.tiger_name = tiger_name
-        self.tiger_diet_plan = tiger_diet_plan
-        self.tiger_outing = tiger_outing
-        self.tiger_env = tiger_env
+    def set_attributes(self, age, color, gender, breed, mood):
+        self.Tiger_age = age
+        self.Tiger_color = color
+        self.Tiger_gender = gender
+        self.Tiger_breed = breed
+        self.Tiger_mood = mood
 
-    def tiger_memory_address(self):
-        print(f'Tiger is located in this {id(self)} address')
+    def set_details(self, name, diet_plan, outing, environment):
+        self.tiger_name = name
+        self.tiger_diet_plan = diet_plan
+        self.tiger_outing = outing
+        self.tiger_env = environment
+
+    def __str__(self):
+        return f"Tiger(name={self.tiger_name}, age={self.Tiger_age}, color={self.Tiger_color}, gender={self.Tiger_gender}, breed={self.Tiger_breed}, mood={self.Tiger_mood}, diet_plan={self.tiger_diet_plan}, outing={self.tiger_outing}, environment={self.tiger_env})"
+
+    def get_memory_address(self):
+        print(f'Tiger is located at memory address: {id(self)}')
 
     def tiger_report(self):
         print(f'''
-        Tiger Report is
-        Tiger name is {self.tiger_name}
-        Tiger diet plan is {self.tiger_diet_plan}
-        Tiger Environment is {self.tiger_env}
+        Tiger Report:
+        Tiger name: {self.tiger_name}
+        Tiger diet plan: {self.tiger_diet_plan}
+        Tiger Environment: {self.tiger_env}
         ''')
 
-
 obj1 = Tiger()
-obj1.Tiger_attributes('2 Months', 'black', 'male', 'chicken', 'aggressive')
-obj1.our_tiger('jack', '3 / 3', 'Commercial', 'garden')
-obj1.tiger_memory_address()
+obj1.set_attributes('2 Months', 'black', 'male', 'chicken', 'aggressive')
+obj1.set_details('jack', '3 / 3', 'Commercial', 'garden')
+obj1.get_memory_address()
 obj1.tiger_report()
 print(f'obj1 Address is {id(obj1)}')
